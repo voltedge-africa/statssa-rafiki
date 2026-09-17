a93f49f6b1ed80e4b9114d8e74c8a111558f97d2
Sipho Mokoena <siphowilliammokoena@gmail.com>
2026-09-17 16:05:12 +0200

    feat: vp create all required apps

---
+++ packages/ui/vite.config.ts
@@ -0,0 +1,17 @@
+import { defineConfig } from "vite-plus";
+
+export default defineConfig({
+  pack: {
+    dts: {
+      generator: "tsgo",
+    },
+    exports: true,
+  },
+  lint: {
+    options: {
+      typeAware: true,
+      typeCheck: true,
+    },
+  },
+  fmt: {},
+});
